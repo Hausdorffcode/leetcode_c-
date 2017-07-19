3869e3635869daada09df63680ab814b968ad4d5
Hausdorffcode <m15950172843@163.com>
2017-07-19 23:07:59 +0800

    dfs

--- Palindrome Partitioning.cpp
+++ Palindrome Partitioning.cpp
@@ -1,2 +1,43 @@
 //https://leetcode.com/problems/palindrome-partitioning/#/description
 
+//dfs
+//time O(2^n), space O(n)
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ret;
+        vector<string> path;
+        dfs(ret, path, s, 0, 1);
+        return ret;
+    }
+    
+private:
+    void dfs(vector<vector<string>> &ret, vector<string> &path, string &s, int prev, int cur) {
+        if (cur == s.size()) {
+            if (isP(s, prev, cur-1)) {
+                path.push_back(s.substr(prev, cur-prev));
+                ret.push_back(path);
+                path.pop_back();   //重要！
+            }
+            return;
+        }
+        
+        if (isP(s, prev, cur-1)) {
+            path.push_back(s.substr(prev, cur-prev));
+            dfs(ret, path, s, cur, cur+1);
+            path.pop_back();
+        }
+        dfs(ret, path, s, prev, cur+1);
+    }
+    
+    bool isP(string s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
--- 海岛.cpp
+++ 海岛.cpp
@@ -1,27 +1,50 @@
 //http://hihocoder.com/contest/hiho156/problem/1
 
-//dfs
-#include<cstdio>
-#include<cstring>
-const int maxn = 60;
+//dfs，用相对位置判断岛屿形状
+#include <bits/stdc++.h>
+using namespace std;
 
-char pic[maxn][maxn];
-int visited[maxn][maxn];
-int dx[] = {0, 0, -1, 1};
-int dy[] = {-1, 1, 0, 0};
+const int INF = 0x3f3f3f3f;
+const double eps = 1E-6;
+const int MAXN = 60;
+
+char pic[MAXN][MAXN];
+int visited[MAXN][MAXN];
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 int N, M;
+vector<int> area;
+set<vector<pair<int, int> > > locs;
 
-void dfs(int row, int col, int cnt) {
+void dfs(int row, int col, int cnt, int &a, vector<pair<int, int> > &loc, int rx, int ry) {
     visited[row][col] = cnt;
+    a++;
+    loc.push_back(make_pair(row-rx, col-ry));
     for (int i = 0; i < 4; i++) {
         if (row+dx[i] >= 0 && row+dx[i] < N && col+dy[i] >= 0 && col+dy[i] < M && pic[row+dx[i]][col+dy[i]] == '#' && visited[row+dx[i]][col+dy[i]] == 0) {
-            dfs(row+dx[i], col+dy[i], cnt);
+            dfs(row+dx[i], col+dy[i], cnt, a, loc, rx, ry);
         }
     }
 }
 
-int main() {
-    freopen("input.txt", "r", stdin);
+bool isSame(vector<pair<int, int> > &c1, vector<pair<int, int> > &c2) {
+    int n = c1.size();
+    if (n == 0) return true;
+    int dx = c1[0].first - c2[0].first;
+    int dy = c1[0].second - c2[0].second;
+    bool ok = true;
+    for (int i = 1; i < n; i++) {
+        if (dx != c1[i].first - c2[i].first || dy != c1[i].second - c2[i].second) {
+            ok = false;
+            break;
+        }
+    }
+    return ok;
+}
+
+int main()
+{
+    freopen("test.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
     int cnt = 0;
     scanf("%d %d", &N, &M);
@@ -34,12 +57,22 @@ int main() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             if (visited[i][j] == 0 && pic[i][j] == '#') {
-                dfs(i, j, ++cnt);
+                int a = 0;
+                vector<pair<int, int> > loc;
+                dfs(i, j, ++cnt, a, loc, i, j);
+                area.push_back(a);
+                locs.insert(loc);
             }
         }
     }
 
-    printf("%d\n", cnt );
 
-  return 0;
+
+    int ac = 0;
+    sort(area.begin(), area.end());
+    for (int i = 0; i < area.size(); i++) {
+        if (!i || area[i] != area[i-1]) ac++;
+    }
+    printf("%d %d %d\n", cnt, ac, locs.size());
+    return 0;
 }
